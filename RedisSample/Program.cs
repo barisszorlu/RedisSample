8e93e2d35736627d74a8219646ef6df4dfd384db
Barış Zorlu <34623086+barisszorlu@users.noreply.github.com>
2022-11-05 22:03:05 +0300

    program.cs edited

--- RedisSample/Program.cs
+++ RedisSample/Program.cs
@@ -7,11 +7,10 @@ IConfiguration configuration = builder.Configuration;
 var multiplexer = ConnectionMultiplexer.Connect(configuration.GetConnectionString("Redis"));
 builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
 builder.Services.AddSingleton<ICacheService, RedisCacheService>();
+builder.Services.AddSingleton<ICategoryService, CategoryService>();
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
